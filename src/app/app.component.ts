149449686cf04136c58c6536cd3f7f1eac4cd794
prusotamkrydv78 <prusotamkumaryadav78@gmail.com>
2024-06-15 07:48:26 -0400

    about section completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeSectionComponent } from './Component/home-section/home-section.component';
-
+import { HomeSectionComponent, } from './Component/home-section/home-section.component';
+import {AboutSectionComponent} from './Component/about-section/about-section.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeSectionComponent],
+  imports: [RouterOutlet,HomeSectionComponent,AboutSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
