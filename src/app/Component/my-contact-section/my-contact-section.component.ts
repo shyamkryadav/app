78e244502eaf9ae9b54347c1b40060531b685f39
shyamkryadav <shyamkryadav345@gmail.com>
2024-06-16 23:36:15 +0900

    form data  bind with tempate driven

--- src/app/Component/my-contact-section/my-contact-section.component.ts
+++ src/app/Component/my-contact-section/my-contact-section.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-my-contact-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './my-contact-section.component.html',
   styleUrl: './my-contact-section.component.css'
 })
 export class MyContactSectionComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log(form.value);
+      // Handle form submission logic here
+    }
+  }
 
 }
